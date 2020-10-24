ce00f1064d06aa1c1a9f2e94ac2671de15317b18
Jessie Aniguid <jessieaniguid@gmail.com>
2020-10-24 16:06:57 -0700

    Final fix I swear

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,5 +4,50 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] private List<IInteractable> playerInventory;
+    public static GameManager Instance;
+
+    [SerializeField] private List<ICollectible> playerInventory;
+
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+        if(playerInventory == null) playerInventory = new List<ICollectible>();
+
+    }
+
+    public void AddToInventory(ICollectible collectible)
+    {
+        playerInventory.Add(collectible);
+    }
+
+    public void RemoveFromInventory(ICollectible collectible)
+    {
+        playerInventory.Remove(collectible);
+    }
+
+    public List<ICollectible> GetInventory()
+    {
+        return playerInventory;
+    }
+
+    public void PrintInventory()
+    {
+        foreach(ICollectible col in playerInventory)
+        {
+            print(col.GetItemName());
+        }
+
+        print("Done printing inventory");
+    }
 }
